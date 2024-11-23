5fa0aaa584ede3aa89c77f506feb5ae5a0dc265a
Umar <shumarb@outlook.com>
2024-11-23 18:47:22 +0800

    Added Javadoc documentation

--- src/main/java/com/example/gamevault/model/Person.java
+++ src/main/java/com/example/gamevault/model/Person.java
@@ -1,3 +1,6 @@
+/**
+ * Abstract class serving as a base class for different types of users of the application.
+ */
 package com.example.gamevault.model;
 
 import jakarta.persistence.*;
@@ -12,20 +15,42 @@ import lombok.Setter;
 @Setter
 public abstract class Person {
 
+    /**
+     * Identification number attribute of a Person entity.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    /**
+     * Name attribute of a Person entity.
+     */
     private String name;
 
+    /**
+     * Username attribute of a Person entity.
+     */
     @Column(unique = true)
     private String username;
 
+    /**
+     * Email address attribute of a Person entity.
+     */
     @Column(unique = true)
     private String email;
 
+    /**
+     * Password attribute of a Person entity.
+     */
     private String password;
 
+    /**
+     * Constructor to instantiate a Person entity.
+     * @param name      The name of the Person entity.
+     * @param username  The username of the Person entity.
+     * @param email     The email address of the Person entity.
+     * @param password  The password of the Person entity.
+     */
     public Person(String name, String username, String email, String password) {
         this.name = name;
         this.username = username;
@@ -33,6 +58,10 @@ public abstract class Person {
         this.password = password;
     }
 
+    /**
+     * Abstract method to get the role of the Person entity, to be implemented by its subclasses.
+     * @return The role of the Person entity.
+     */
     public abstract String getRole();
 
 }
